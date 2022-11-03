10ba3e98331d063318cb5bdd69b7bd2f0b4edff3
lim-kim930 <1625753207lim@gmail.com>
2022-11-03 15:30:04 +0800

    some update

--- test/index.ts
+++ test/index.ts
@@ -1,20 +1,34 @@
-import LimQqwry, { toJson } from "../dist";
+import LimQqwry, { toJson, getStartIpInt, ipToInt, intToIP } from "../dist";
 import path from "path";
 import fs from "fs";
 
 const datapath = path.join(__dirname, './qqwry.dat');
 
+const beforeMem = process.memoryUsage().arrayBuffers;
 console.time("time_load_data");
 const qqwry = new LimQqwry(datapath);
 console.timeEnd("time_load_data");
+const memIncrease = (process.memoryUsage().arrayBuffers - beforeMem) / 1024 / 1024;
+console.log("Memory increased: ", memIncrease);
+
 
 console.time("time_single_search");
-const ipdata = qqwry.searchIP("115.120.105.66");
-console.log(ipdata);
+const ipdata = qqwry.searchIP("223.5.5.5");
 console.timeEnd("time_single_search");
+console.log(ipdata);
+
+const intList = qqwry.getStartIpIntList();
+const ip = ipToInt("223.5.5.5");
+console.time("time_get_startip");
+const int = getStartIpInt(ip, intList);
+console.timeEnd("time_get_startip");
+console.log(int);
 
 console.time("time_to_json");
 const result = toJson(datapath);
-fs.writeFile("./data.json", JSON.stringify(result), () => {
-    console.timeEnd("time_to_json");
+console.timeEnd("time_to_json");
+fs.writeFile("./data.json", JSON.stringify(result), error => {
+    if (error) {
+        console.error(error);
+    }
 });
